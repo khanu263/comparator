c8a82858ad1f6bb86e9cf7ceb8a7e7fad8a749bf
Umair Khan <ukn@comcast.net>
2020-06-03 15:31:44 -0700

    Add simple Express server

---
+++ server/server.js
@@ -0,0 +1,19 @@
+// Set up express
+const path = require("path");
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Set up build path
+const buildPath = path.join(__dirname, "..", "build");
+app.use(express.static(buildPath));
+
+// Set up server
+app.get("*", (req, res) => {
+  res.sendFile(path.join(buildPath, "index.html"));
+});
+
+// Set up listener
+app.listen(port, () => {
+  console.log(`Server running on port ${port}.`);
+});
