0a402520d7f6a051e41982bc80bd8598e4e818de
Umair Khan <ukn@comcast.net>
2020-06-03 23:44:17 -0700

    Move favicon management to Express server

--- server/server.js
+++ server/server.js
@@ -6,12 +6,16 @@
 // Set up express
 const path = require("path");
 const express = require("express");
+const favicon = require("express-favicon");
 const app = express();
 const port = process.env.PORT || 3000;
 
 // Set up build path
 const buildPath = path.join(__dirname, "..", "build");
 app.use(express.static(buildPath));
+
+// Set up favicon
+app.use(favicon(path.join(__dirname, "..", "build", "favicon.ico")));
 
 // Set up server
 app.get("*", (req, res) => {
